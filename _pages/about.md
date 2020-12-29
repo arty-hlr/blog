ed83aab69cf504ef83020f5b9b73b50fea8595cf
florian <flrn.pjd@protonmail.com>
2020-12-29 02:08:35 +0100

    added some stuff

--- _pages/about.md
+++ _pages/about.md
@@ -4,8 +4,8 @@ title: About Me
 permalink: /about/
 ---
 
-I am a security consultant with a focus on IoT reversing, OSCP certified pentester, and maths student.
+I am a security consultant, OSCP certified pentester.
 
-I enjoy reverse engineering and exploiting binaries on different architectures and working on programming projects. In my spare time you'll find me reading, CTFing, or at my local CrossFit box. Currently working on my eLearnSecurity XDS and WAPT courses!
+I enjoy reverse engineering and exploiting binaries on different architectures and working on programming projects. In my spare time you'll find me reading, CTFing, or at my local CrossFit box.
 
 You can find me on [GitHub](https://github.com/arty-hlr) or Discord (arty-hlr#1427).
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "OSCP",
+        "excerpt":"So, first blog post had to be about OSCP even if I passed it two months ago, or my buddies in the Infosec Prep discord server would be mad! So here we go, short(ish) review of PWK (the course), OSCP (the exam), what preparation I had, a few pieces of...","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2019/12/10/oscp/",
+        "teaser":null},{
+        "title": "Shellcode",
+        "excerpt":"Intro So I was practicing my shellcode writing skills for XDS from eLearnSecurity, and as I was working through the first basic assembly examples, I realized searching for syscalls numbers for different architectures was pretty tedious, like what’s the syscall number for execve in the i386 or ARM architectures, or...","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2019/12/29/shellcode/",
+        "teaser":null}]
