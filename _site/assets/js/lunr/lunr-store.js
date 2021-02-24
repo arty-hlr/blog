14005644252e25ee8e482ac0dc3b04e7c0203754
florian <flrn.pjd@protonmail.com>
2021-02-24 15:31:37 +0100

    added eCXD and Offshore posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,7 +5,32 @@ var store = [{
         "url": "http://localhost:4000/blog/2019/12/10/oscp/",
         "teaser":null},{
         "title": "Shellcode",
-        "excerpt":"Intro So I was practicing my shellcode writing skills for XDS from eLearnSecurity, and as I was working through the first basic assembly examples, I realized searching for syscalls numbers for different architectures was pretty tedious, like what’s the syscall number for execve in the i386 or ARM architectures, or...","categories": ["blog"],
+        "excerpt":"So I was practicing my shellcode writing skills for XDS from eLearnSecurity, and as I was working through the first basic assembly examples, I realized searching for syscalls numbers for different architectures was pretty tedious, like what’s the syscall number for execve in the i386 or ARM architectures, or what’s...","categories": ["blog"],
         "tags": [],
         "url": "http://localhost:4000/blog/2019/12/29/shellcode/",
+        "teaser":null},{
+        "title": "eLearnSecurity Certified eXploit Developer",
+        "excerpt":"Update 25.01.2021 In the year or so since this blog post, eLS has been acquired by INE, the courses are now only available as a subscription on INE’s website. Also, OffSec has launched a basic exploit dev course of their own here focused on windows, which still doesn’t cover x64...","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2020/12/15/ecxd/",
+        "teaser":null},{
+        "title": "Offshore",
+        "excerpt":"Intro December was gonna be another month of lockdown at home without much to do, so after a friend pwned it, and some nice troll from the InfoSec Prep discord started it, I decided to follow suit and to dedicate the month to Offshore! Unfortunately I was a bit impatient,...","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2020/12/25/offshore/",
+        "teaser":null},{
+        "title": "Ewptx",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2021/02/01/ewptx.html",
+        "teaser":null},{
+        "title": "Crte",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2021/02/15/crte.html",
+        "teaser":null},{
+        "title": "Blue Team",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2021/02/24/blue-team.html",
         "teaser":null}]
